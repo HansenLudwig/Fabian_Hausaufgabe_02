afda6facd13ea7cdd95710248eb0bdcb8d55838b
Fabian Tschirschnitz <fabian.tschirchnitz@dsc-gmbh.com>
2023-09-12 17:36:50 +0200

    remove solution from master ;)

--- server/index.js
+++ server/index.js
@@ -22,29 +22,7 @@ app.listen(port, () => {
 });
 
 function analyzeData(aData) {
-  const meterReadingsMappedAndChecked = aData
-    .map((reading) => {
-      reading.reading_date = new Date(reading.reading_date);
-      return reading;
-    })
-    .map((reading, index, meterReadings) => {
-      const sortedMeterReadingOfSameCustomer = meterReadings
-        .filter((x) => x.customer_id === reading.customer_id)
-        .sort((a, b) => a.reading_date - b.reading_date);
-      const earlierReadings = sortedMeterReadingOfSameCustomer.slice(
-        0,
-        sortedMeterReadingOfSameCustomer.indexOf(reading)
-      );
-      if (
-        earlierReadings.some(
-          (otherReading) => otherReading.meter_reading >= reading.meter_reading
-        )
-      ) {
-        reading.status = "Warning";
-      } else {
-        reading.status = "Okay";
-      }
-      return reading;
-    });
+  const meterReadingsMappedAndChecked = [...aData];
+  //TODO Implement this function to detect malicious meter readings in aData
   return meterReadingsMappedAndChecked;
 }
